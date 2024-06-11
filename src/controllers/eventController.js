47ffa80afe517aabf64930ec1e4914c80685965f
Debarshi Das <debarshi121@gmail.com>
2024-06-11 14:14:22 +0530

    create event

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -59,9 +59,35 @@ const uploadEventThumbnail = asyncHandler(async (req, res) => {
 	return res.status(200).json(new ApiResponse(200, {url: thumbnail?.url}, "Avatar image updated successfully"));
 });
 
+const createEvent = asyncHandler(async (req, res) => {
+	const event = {
+		title: "Test title",
+		categories: [1, 2],
+		description: "Test",
+		thumbnail: "http://res.cloudinary.com/dv68nyejy/image/upload/v1718091388/evento/thumbnails/yn9uayc13x3jbbfy1kvg.jpg",
+		banner: "http://res.cloudinary.com/dv68nyejy/image/upload/v1718091397/evento/banners/ls7bd8wmsrxhvzvfa5ci.png",
+		videoUrl: "https://www.youtube.com/watch?v=17j-dGb3oO0",
+		venue: "AEI Field",
+		startDate: "2024-06-11T07:37:24.300Z",
+		endDate: "2024-06-11T07:37:26.300Z",
+		entryFee: "100",
+		lat: 26.183959,
+		lng: 91.743094,
+		organizerName: "News Live",
+		organizerEmail: "testssdsds@mail.com",
+		organizerPhone: "123123312",
+		plan: "BASIC",
+	};
+
+	setTimeout(() => {
+		return res.status(200).json(new ApiResponse(200, {event}, "Avatar image updated successfully"));
+	}, 2000);
+});
+
 module.exports = {
 	getSingleEvent,
 	getAllEvents,
 	uploadEventBanner,
 	uploadEventThumbnail,
+	createEvent,
 };
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getSingleEvent, getAllEvents, uploadEventBanner, uploadEventThumbnail} = require("../controllers/eventController");
+const {getSingleEvent, getAllEvents, uploadEventBanner, uploadEventThumbnail, createEvent} = require("../controllers/eventController");
 const {verifyJWT, verifyRoles} = require("../middlewares/authMiddleware");
 const {UserRolesEnum} = require("../utils/constants");
 const upload = require("../middlewares/multerMiddleware");
@@ -7,6 +7,7 @@ const upload = require("../middlewares/multerMiddleware");
 const router = express.Router();
 
 router.get("/", verifyJWT, getAllEvents);
+router.post("/", verifyJWT, createEvent);
 router.get("/:id", getSingleEvent);
 router.post("/upload-banner", upload.single("banner"), uploadEventBanner);
 router.post("/upload-thumbnail", upload.single("thumbnail"), uploadEventThumbnail);
